87b4cf6b94770301558ed614b3b8c6cc4cae658f
Joash Kikoso <151738520+truebornsleet@users.noreply.github.com>
2024-11-01 10:34:45 +0300

    Add files via upload

---
+++ clone 4/js/script.js
@@ -0,0 +1,26 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const contentItems = document.querySelectorAll('.tab-content-item');
+
+
+
+function selectItem(e) {
+    removeBorder();
+    removeShow();
+    this.classList.add('tab-border');
+    const tabContentItem = document.querySelector(`#${this.id}-content`);
+    tabContentItem.classList.add('show');
+     
+}
+
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+function removeShow() {
+    contentItems.forEach(item => item.classList.remove('show'));
+}
+
+tabItems.forEach(item => {
+    item.addEventListener('click', selectItem);
+})
